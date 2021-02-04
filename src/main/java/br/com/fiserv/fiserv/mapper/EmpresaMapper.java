28f3a7294ddfe3cc74c563e795ea5df29bbd513a
goesbernardo <bernardo.goes01@gmail.com>
2021-02-04 13:47:39 -0300

    ajuste mapper

--- src/main/java/br/com/fiserv/fiserv/mapper/EmpresaMapper.java
+++ src/main/java/br/com/fiserv/fiserv/mapper/EmpresaMapper.java
@@ -3,16 +3,31 @@ package br.com.fiserv.fiserv.mapper;
 import br.com.fiserv.fiserv.dto.EmpresaDTO;
 import br.com.fiserv.fiserv.model.Empresa;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface EmpresaMapper {
 
 
     EmpresaMapper INSTANCE = Mappers.getMapper(EmpresaMapper.class);
 
+
+    @Mappings(
+            value = {
+                    @Mapping(source = "nome", target = "nome"),
+                    @Mapping(source = "cnpj", target = "cnpj")
+            }
+    )
     Empresa toModel(EmpresaDTO empresaDTO);
 
+    @Mappings(
+            value = {
+                    @Mapping(source = "nome", target = "nome"),
+                    @Mapping(source = "cnpj", target = "cnpj")
+            }
+    )
     EmpresaDTO toDTO(Empresa empresa);
 
 
